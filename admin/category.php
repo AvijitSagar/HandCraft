6d2e9313c51e7da1f047097ec1e1bee7e477c8b0
saggarr-sagar <125144690+saggarr-work@users.noreply.github.com>
2023-02-18 19:48:01 +0600

    admin panel design

---
+++ admin/category.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- -----head include---- -->
+    <?php include("partials/head.php"); ?>
+    <title>Admin Category</title>
+</head>
+<body>
+    <!-- --------include menu------- -->
+    <?php include("partials/menu.php"); ?>
+
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Category</h1>
+        </div>
+    </div>
+
+
+    <!-- -------------include footer--------- -->
+    <?php include("partials/footer.php"); ?>
+</body>
+</html>
