d10f5d590c289f58841432bb2ae44bc46420872e
saggarr-sagar <125144690+saggarr-work@users.noreply.github.com>
2023-02-19 12:54:31 +0600

    db operation

--- admin/order.php
+++ admin/order.php
@@ -12,6 +12,51 @@
     <div class="main-content">
         <div class="wrapper">
             <h1>Manage Order</h1>
+            <br><br><br><br><br>
+            <table class="tbl-full">
+                <tr>
+                    <th>Sl.No</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Action</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>sagar biswas</td>
+                    <td>saggarr</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update</a>
+                        <a href="#" class="btn-danger">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>sagar biswas</td>
+                    <td>saggarr</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update</a>
+                        <a href="#" class="btn-danger">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>sagar biswas</td>
+                    <td>saggarr</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update</a>
+                        <a href="#" class="btn-danger">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>sagar biswas</td>
+                    <td>saggarr</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update</a>
+                        <a href="#" class="btn-danger">Delete</a>
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
 
